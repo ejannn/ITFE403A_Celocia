0376c15a43dc695639bd62da431c77ec5197d51d
ejannn <nieljhon.celocia@dbtc-cebu.edu.ph>
2026-08-04 09:31:56 +0800

    docs: added footer components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
