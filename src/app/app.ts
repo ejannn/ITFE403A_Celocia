a7a2eca93740bc5bc15c33a26f5765428306cca4
ejannn <nieljhon.celocia@dbtc-cebu.edu.ph>
2026-08-04 09:51:03 +0800

    docs: added component profile-card

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+import { ProfileCard } from "./components/profile-card/profile-card";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, Footer],
+  imports: [Header, Footer, ProfileCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
