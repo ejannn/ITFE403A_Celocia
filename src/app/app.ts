d9a8df367ac8c0545757a3f1d0db5c18bd3103ab
ejannn <nieljhon.celocia@dbtc-cebu.edu.ph>
2026-08-04 10:17:07 +0800

    docs: added components announcement-card

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { ProfileCard } from "./components/profile-card/profile-card";
+import { AnnouncementCard } from "./components/announcement-card/announcement-card";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, Footer, ProfileCard],
+  imports: [Header, Footer, ProfileCard, AnnouncementCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
