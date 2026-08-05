8a840df06204994feab7043de3bb926adcef0228
ejannn <nieljhon.celocia@dbtc-cebu.edu.ph>
2026-08-05 10:40:10 +0800

    refactor: changed static to dynamic student data

--- src/app/components/profile-card/profile-card.ts
+++ src/app/components/profile-card/profile-card.ts
@@ -6,4 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './profile-card.html',
   styleUrl: './profile-card.css',
 })
-export class ProfileCard {}
+export class ProfileCard {
+
+StudentFirstName: string = 'Niel Jhon';
+StudentMiddleName: string = 'Evardo';
+StudentLastName: string = 'Celocia';
+StudentNumber: string = '2023-00000';
+Course: string = 'BSIT';
+YearLevel: string = '4th Year';
+Section: string = 'Valorant';
+Nickname: string = 'pako';
+Grade: number = 2.5;
+
+get status(): string {
+    return (this.Grade >= 1.0 && this.Grade <= 3.0) ? 'Pass' : 'Fail';
+  }
+
+
+}
+
+
+
